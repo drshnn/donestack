4fd3f51b7025ef1ab9cd3d230a7df35bbddd8b06
drshnn <darshansu156@gmail.com>
2022-07-28 12:32:48 +0530

    auth check functions

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import authUtils from "../../utils/authUtils";
+import Spinner from "./common/Spinner";
 
 function AuthLayout() {
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+  //check authentication
+  const checkAuth = async () => {
+    const isAuth = await authUtils.isAuthenticated();
+    if (!isAuth) {
+      setLoading(false);
+    } else {
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, [navigate]);
+
+  return loading ? <p>loading</p> : <Outlet />;
 }
 
 export default AuthLayout;
