cd753a84519dc167562af4b291c5e0ae0e966f20
drshnn <darshansu156@gmail.com>
2022-07-30 08:39:52 +0530

    authorization

--- client/src/redux/features/user/userAction.js
+++ client/src/redux/features/user/userAction.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-const baseURL = "http://localhost:5000/api/auth/"
+const baseURL = "http://localhost:5000/api/"
 export const register = createAsyncThunk(
     'user/register',
     async ({username,email,password},{rejectWithValue})=>{
@@ -10,7 +10,7 @@ export const register = createAsyncThunk(
                 'Content-Type':"application/json"
             },
            }
-      const {data}  =   await axios.post(baseURL+'register',{username,email,password},config)
+      const {data}  =   await axios.post(baseURL+'auth/register',{username,email,password},config)
       localStorage.setItem('userToken', data.token)
       return data
         } catch (error) {
@@ -34,7 +34,7 @@ export const login = createAsyncThunk(
         },
       }
       const { data } = await axios.post(
-        baseURL+'login',
+        baseURL+'auth/login',
         { email, password },
         config
       )
@@ -43,6 +43,31 @@ export const login = createAsyncThunk(
       return data
     } catch (error) {
       // return custom error message from API if any
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message)
+      } else {
+        return rejectWithValue(error.message)
+      }
+    }
+  }
+)
+
+export const getUserDetails = createAsyncThunk(
+  'user/getUserDetails',
+  async (arg, { getState, rejectWithValue }) => {
+    try {
+      // get user data from store
+      const { user } = getState()
+
+      // configure authorization header with user's token
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.userToken}`,
+        },
+      }
+      const { data } = await axios.get(baseURL+'private', config)
+      return data
+    } catch (error) {
       if (error.response && error.response.data.message) {
         return rejectWithValue(error.response.data.message)
       } else {
